c6e963e91288a1ed2a342c3d216172acd22cf9b7
T.Sanyi <s.truczkai@gmail.com>
2023-08-12 15:53:27 +0200

    add firebase database to the app

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-f9fa0-default-rtdb.europe-west1.firebasedatabase.app/" //process.env.DATABASE_URL
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const itemsInDB = ref(database, "items")
+
+console.log(app)
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
-    console.log(inputFieldEl.value)
+    push(itemsInDB, inputFieldEl.value)
 })
